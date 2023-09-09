077c7a53e81204bd96d174f8a5e072f08dea8c5a
liuyulvv <liuyulvv@outlook.com>
2023-09-09 16:19:34 +0800

    initial project

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'eslint-config-prettier'
+    ],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script'
+            }
+        }
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        camelcase: [
+            'error',
+            {
+                properties: 'always'
+            }
+        ]
+    }
+};
