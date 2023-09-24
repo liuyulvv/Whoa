52dbb0263ff6dfc5acb77fbe42ac4d0db520f63e
liuyulvv <liuyulvv@outlook.com>
2023-09-24 10:27:19 +0800

    del: namespace

--- WhoaEvent/src/WhoaEvent.ts
+++ WhoaEvent/src/WhoaEvent.ts
@@ -1,43 +1,41 @@
-export namespace Whoa {
-    export class WhoaEvent {
-        private static instance: WhoaEvent;
-        private subscribers: { [key: string]: Array<() => void> };
+export class WhoaEvent {
+    private static instance: WhoaEvent;
+    private subscribers: { [key: string]: Array<() => void> };
 
-        private constructor() {
-            this.subscribers = {};
+    private constructor() {
+        this.subscribers = {};
+    }
+
+    public static get() {
+        if (!WhoaEvent.instance) {
+            WhoaEvent.instance = new WhoaEvent();
         }
+        return WhoaEvent.instance;
+    }
 
-        public static get() {
-            if (!WhoaEvent.instance) {
-                WhoaEvent.instance = new WhoaEvent();
-            }
-            return WhoaEvent.instance;
+    public sub(key: string, callback: () => void) {
+        if (!this.subscribers[key]) {
+            this.subscribers[key] = [];
         }
+        this.subscribers[key].push(callback);
+    }
 
-        public sub(key: string, callback: () => void) {
-            if (!this.subscribers[key]) {
-                this.subscribers[key] = [];
-            }
-            this.subscribers[key].push(callback);
-        }
-
-        public unsub(key: string, callback: () => void) {
-            if (this.subscribers[key]) {
-                const index = this.subscribers[key].indexOf(callback);
-                if (index != -1) {
-                    this.subscribers[key].splice(index, 1);
-                }
+    public unsub(key: string, callback: () => void) {
+        if (this.subscribers[key]) {
+            const index = this.subscribers[key].indexOf(callback);
+            if (index != -1) {
+                this.subscribers[key].splice(index, 1);
             }
         }
+    }
 
-        public pub(key: string) {
-            if (this.subscribers[key]) {
-                this.subscribers[key].forEach((callback) => {
-                    callback();
-                });
-            }
+    public pub(key: string) {
+        if (this.subscribers[key]) {
+            this.subscribers[key].forEach((callback) => {
+                callback();
+            });
         }
     }
 }
 
-window.WhoaEvent = Whoa.WhoaEvent.get();
+window.WhoaEvent = WhoaEvent.get();
