b83ed68b96fbce388f12a5ddf93a3a4371d3427f
liuyulvv <liuyulvv@outlook.com>
2023-10-04 21:37:51 +0800

    fix: 增加Whoa命名空间

--- WhoaFramework/src/index.ts
+++ WhoaFramework/src/index.ts
@@ -2,11 +2,12 @@ import Entity from './entities/Entity';
 import EntityRole from './entities/EntityRole';
 import EntityType from './entities/EntityType';
 
-const WhoaFramework = {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(<any>window).Whoa = {};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(<any>window).Whoa.WhoaFramework = {
     Entity: Entity,
     EntityRole: EntityRole,
     EntityType: EntityType
 };
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(<any>window).WhoaFramework = WhoaFramework;
