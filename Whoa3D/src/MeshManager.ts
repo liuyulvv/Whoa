cfd38eadc88472be90e7386b84a597615f5be25f
liuyulvv <liuyulvv@outlook.com>
2023-10-05 13:56:21 +0800

    update: Ground的材质设置

--- Whoa3D/src/MeshManager.ts
+++ Whoa3D/src/MeshManager.ts
@@ -13,6 +13,10 @@ export default class MeshManager {
     public createGround(): Mesh {
         const mesh = this.builder.CreateGround('ground', { width: 100, height: 100 }, this.scene);
         const material = new GridMaterial('groundMaterial', this.scene);
+        material.majorUnitFrequency = 5;
+        material.minorUnitVisibility = 0.5;
+        material.gridRatio = 1;
+        material.useMaxLine = true;
         material.opacity = 0.99;
         mesh.material = material;
         return new Mesh(mesh, material);
