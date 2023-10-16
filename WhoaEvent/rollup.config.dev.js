f24345afd911b0260a903b565926eebc9ffd93b5
liuyulvv <liuyulvv@outlook.com>
2023-10-16 10:24:26 +0800

    update: 增加rrollup dev配置

---
+++ WhoaEvent/rollup.config.dev.js
@@ -0,0 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: {
+            file: '../dist/script/WhoaEvent.js',
+            format: 'es',
+            sourcemap: true
+        },
+        plugins: [resolve(), typescript({ module: 'ESNext' })]
+    }
+];
---
+++ WhoaUI/rollup.config.dev.js
@@ -0,0 +1,36 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy';
+import postcss from 'rollup-plugin-postcss';
+
+const GLOBAL = `const process = {
+    env: {
+        NODE_ENV: 'production'
+    }
+};`;
+
+export default {
+    input: 'src/main.tsx',
+    output: {
+        file: '../dist/script/WhoaUI.js',
+        format: 'es',
+        sourcemap: true,
+        banner: GLOBAL
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        json(),
+        postcss(),
+        typescript({ module: 'ESNext' }),
+        copy({
+            targets: [{ src: 'public/index.html', dest: '../dist/' }]
+        })
+    ],
+    onwarn(warning, warn) {
+        if (warning.code === 'CIRCULAR_DEPENDENCY') return;
+        warn(warning);
+    }
+};
