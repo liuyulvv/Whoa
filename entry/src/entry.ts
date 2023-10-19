6de5f5d544be8a85415baa72cb492650b8ab7dd6
liuyulvv <liuyulvv@outlook.com>
2023-10-19 19:11:46 +0800

    update: 修改编译脚本

--- entry/src/entry.ts
+++ entry/src/entry.ts
@@ -1,11 +1,4 @@
-const modules = [
-    './WhoaEvent.js',
-    './WhoaUI.js',
-    './Whoa3D.js',
-    './WhoaFramework.js',
-    './WhoaGeometry.js',
-    './WhoaInteraction.js'
-];
+const modules = ['./WhoaEvent.js', './WhoaUI.js', './WhoaFramework.js', './WhoaGeometry.js', './WhoaInteraction.js'];
 
 async function load() {
     for (const module of modules) {
