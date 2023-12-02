c90f3dd52648189475c85ebb5f7f24fea5681c58
liuyulvv <liuyulvv@outlook.com>
2023-12-02 19:16:58 +0800

    update: WhoaMath

--- WhoaMath/src/index.ts
+++ WhoaMath/src/index.ts
@@ -1,9 +1,12 @@
 import Color3 from './Color3';
 import Color4 from './Color4';
+import Matrix3 from './Matrix3';
+import Matrix4 from './Matrix4';
 import Point2 from './Point2';
 import Point3 from './Point3';
 import Vector2 from './Vector2';
 import Vector3 from './Vector3';
+import Vector4 from './Vector4';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (<any>window).Whoa = {};
@@ -15,5 +18,8 @@ import Vector3 from './Vector3';
     Point2: Point2,
     Point3: Point3,
     Vector2: Vector2,
-    Vector3: Vector3
+    Vector3: Vector3,
+    Vector4: Vector4,
+    Matrix3: Matrix3,
+    Matrix4: Matrix4
 };
