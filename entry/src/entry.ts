2edf685e5da356e79cab730534c5ef13325712c9
liuyulvv <liuyulvv@outlook.com>
2024-01-04 23:42:58 +0800

    add: WhoaGeometry

--- entry/src/entry.ts
+++ entry/src/entry.ts
@@ -1,4 +1,4 @@
-const modules = ['./WhoaFoundation.js', './WhoaUI.js', './WhoaFramework.js', './WhoaGeneral.js'];
+const modules = ['./WhoaFoundation.js', './WhoaUI.js', './WhoaFramework.js', './WhoaGeometry.js', './WhoaGeneral.js'];
 
 async function load() {
     for (const module of modules) {
